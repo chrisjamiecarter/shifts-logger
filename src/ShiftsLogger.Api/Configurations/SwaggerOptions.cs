54362759d9b27f28119291552de9365b738f069b
Chris Carter <108958713+cjc-sweatbox@users.noreply.github.com>
2024-07-30 14:55:18 +0100

    Implemented database seeding
    #16

--- src/ShiftsLogger.Api/Configurations/SwaggerOptions.cs
+++ src/ShiftsLogger.Api/Configurations/SwaggerOptions.cs
@@ -1,14 +1,16 @@
 ﻿namespace ShiftsLogger.Api.Configurations;
 
 /// <summary>
-/// Class to hold the required application options to run swagger.
+/// Class to hold the required application options for swagger.
 /// </summary>
 public class SwaggerOptions
 {
     #region Properties
 
     public string JsonRoute { get; set; }
+
     public string Description { get; set; }
+
     public string UiEndpoint { get; set; }
 
     #endregion
